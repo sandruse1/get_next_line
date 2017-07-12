58e92197e7c61ed792791fd7c860c27bc1073112
sandruse1 <andrusechko@gmai.com>
2017-07-12 21:17:25 +0300

    get_next_line function

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sandruse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/20 17:35:05 by sandruse          #+#    #+#             */
+/*   Updated: 2017/02/20 11:09:56 by sandruse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# define BUFF_SIZE 2
+
+int		get_next_line(const int fd, char **line);
+
+#endif
